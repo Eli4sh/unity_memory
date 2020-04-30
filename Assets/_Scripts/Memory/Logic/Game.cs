43e3e0982b28f9c06aec12ac945a26bf1a0f251f
Radosław Krystkowiak <eliash.k11@gmail.com>
2020-04-30 12:40:36 +0200

    Create cards scratch

---
+++ Assets/_Scripts/Memory/Logic/Game.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+using UnityEngine;
+
+namespace Memory.Logic
+{
+    public static partial class Game
+    {
+        //TODO: MANAGE PUBLIC/PRIVATE ACCESS
+        //TODO: ACROSS GAME CLASS
+        public static Factory Factory;
+    }
+}
