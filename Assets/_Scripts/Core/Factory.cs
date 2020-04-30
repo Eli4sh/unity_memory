7edb9614729b37ec6fd7e322a992c609df36c31c
Radosław Krystkowiak <eliash.k11@gmail.com>
2020-04-30 12:36:06 +0200

    GameObject Factory

---
+++ Assets/_Scripts/Core/Factory.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class Factory : MonoBehaviour
+    {
+        public GameObject CreateGameObjectFromResources(
+            string path)
+        {
+            return Instantiate(
+                original: Resources.Load<GameObject>(path: path),
+                parent: transform);
+        }
+    }
+}
