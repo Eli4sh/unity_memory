87a673c2a93805233afd02c7c2ec51ea46453bc0
Radosław Krystkowiak <eliash.k11@gmail.com>
2020-04-30 12:37:04 +0200

    Memory game settings

---
+++ Assets/_Scripts/Memory/Settings/GameplaySettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Memory.Settings
+{
+    [CreateAssetMenu(fileName = "Gameplay Settings", menuName = "Memory/Settings/Visual", order = 0)]
+    public class GameplaySettings : ScriptableObject
+    {
+        [Tooltip(tooltip: "Level duration in seconds")]
+        public int LevelDuration;
+
+        [Tooltip(tooltip: "Number of pairs to create in game")]
+        public int MemoryPairs;
+    }
+}
---
+++ Assets/_Scripts/Memory/Settings/VisualSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Memory.Settings
+{
+    [CreateAssetMenu(fileName = "Visual Settings", menuName = "Memory/Settings/Visual", order = 0)]
+    public class VisualSettings : ScriptableObject
+    {
+        public Sprite Background;
+        public Sprite CardFrame;
+        public Sprite[] Animals;
+    }
+}
