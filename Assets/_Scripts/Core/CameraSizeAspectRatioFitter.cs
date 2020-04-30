77a658d2db78114d8de6b37e13b37040505ee95b
Radosław Krystkowiak <eliash.k11@gmail.com>
2020-04-30 17:33:23 +0200

    Camera aspect ratio fitter

---
+++ Assets/_Scripts/Core/CameraSizeAspectRatioFitter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class CameraSizeAspectRatioFitter : MonoBehaviour
+    {
+        private const float _targetAspect = 1.3333f;
+        void Start()
+        {
+            float windowAspect = (float) Screen.height / (float) Screen.width;
+            float scaleSize = windowAspect / _targetAspect;
+            Camera camera = GetComponent<Camera>();
+            camera.orthographicSize *= scaleSize;
+        }
+    }
+}
